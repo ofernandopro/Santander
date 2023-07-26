281d0a5d7f6f87e471292cb316e3187b653bb5df
fernando <fernando@lealapps.com>
2023-07-26 20:18:09 -0300

    Initial Commit

---
+++ Santander/SantanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SantanderApp.swift
+//  Santander
+//
+//  Created by Fernando on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SantanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
